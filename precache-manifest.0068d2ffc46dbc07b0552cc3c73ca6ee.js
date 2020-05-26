bdac3ebaaf53868a75ba4eea2cdc87cb132b075c
miqilin21 <994718917@qq.com>
2020-05-27 01:31:08 +0800

    deploy

---
+++ precache-manifest.0068d2ffc46dbc07b0552cc3c73ca6ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6353a3c2ffa9f3a68824",
+    "url": "/mogu-bill/css/app.17adb4f9.css"
+  },
+  {
+    "revision": "3519777ac0e1c6b4e653c541590b34e3",
+    "url": "/mogu-bill/img/mogu-logo.3519777a.png"
+  },
+  {
+    "revision": "9da38b943a7ad42240a9e670b13d3b07",
+    "url": "/mogu-bill/index.html"
+  },
+  {
+    "revision": "6353a3c2ffa9f3a68824",
+    "url": "/mogu-bill/js/app.9900f98c.js"
+  },
+  {
+    "revision": "f37345a3c6df9146bfb5",
+    "url": "/mogu-bill/js/chunk-vendors.36d9785b.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/mogu-bill/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mogu-bill/robots.txt"
+  }
+]);
